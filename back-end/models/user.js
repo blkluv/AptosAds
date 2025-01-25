fc89b3d6e4b0659da82693c77a0375967a22b4c7
Aman <amananjalitiwari2007@gmail.com>
2025-01-26 01:35:50 +0530

    major restructuring in codebase

---
+++ back-end/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  wallet: {
+    type: String,
+  },
+  createdMemes: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Meme'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
