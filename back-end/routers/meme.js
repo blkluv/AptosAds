93e57b5578b5cb26daa2ae8b6f00b1b688f2becd
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2025-01-26 05:12:25 +0530

    Add meme action and like functionality with user tracking

--- back-end/routers/meme.js
+++ back-end/routers/meme.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { getMemes, createAmeme, getAMeme } = require("../controllers/meme");
+const {
+  getMemes,
+  createAmeme,
+  getAMeme,
+  memeAction,
+  likeMeme,
+} = require("../controllers/meme");
 const router = express.Router();
 
 router.get("/", getMemes);
 router.post("/", createAmeme);
 router.get("/:memeId", getAMeme);
+router.post("/:memeId", memeAction);
+router.post("/:memeId", likeMeme);
 
 module.exports = router;
